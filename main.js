1fa17b1b7103d173fc9e92ba4967d34184d0bbe7
Yeseo Lee <s2019w35@e-mirim.hs.kr>
2021-03-31 10:43:41 +0900

    web2-33

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
 const http = require('http')
 const fs = require('fs')
 const url = require('url')
+const qs = require('querystring')
 
-function templateList(fileList) {
-    var list = '<ul>'
-    var i = 0;
-    while (i < fileList.length) {
-        list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-        i = i + 1;
-    }
-    list = list + '</ul>';
-    return list;
-}
-
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
     return `
           <!doctype html>
           <html lang="ko">
@@ -24,11 +14,20 @@ function templateHTML(title, list, body) {
           <body>
             <h1><a href="/">WEB</a></h1>
             ${list}
-            <a href="/create">create</a>
+            ${control}
             ${body}
           </body>
           </html>
-          `;
+          `
+}
+
+function templateList(filelist) {
+    let list = '<ul>'
+    for (let i = 0; i < filelist.length; i++) {
+        list += `<li><a href="/?id=${filelist[i]}"> ${filelist[i]} </a></li>`
+    }
+    list += '</ul>'
+    return list
 }
 
 const app = http.createServer(function (request, response) {
@@ -39,46 +38,63 @@ const app = http.createServer(function (request, response) {
         if (queryData.id === undefined) {
             const title = 'Welcome'
             const description = 'Hello, Node.js'
+
             fs.readdir('data/', function (err, data) {
-                let list = templateList(data);
-                const template = templateHTML(title, list, `<h2>${title}</h2>${description}`)
+                const list = templateList(data)
+                const template = templateHTML(title, list,
+                    `<h2>${title}</h2>${description}`,
+                    `<a href="/create">create</a>`
+                );
                 response.writeHead(200)
                 response.end(template)
             })
         } else {
             fs.readdir('data/', function (err, data) {
-                let list = '<ul>'
-                for (let i = 0; i < data.length; i++) {
-                    list += `<li><a href="/?id=${data[i]}"> ${data[i]} </a></li>`
-                }
-                list += '</ul>'
-                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-                    const title = queryData.id
-                    let list = templateList(data);
-                    const template = templateHTML(title, list, `<h2>${title}</h2>${description}`)
-                    response.writeHead(200)
-                    response.end(template)
-                })
+                const list = templateList(data)
+                fs.readFile(`data/${queryData.id}`, 'utf8',
+                    function (err, description) {
+                        const title = queryData.id
+                        const template = templateHTML(title, list,
+                            `<h2>${title}</h2>${description}`,
+                            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                        );
+                        response.writeHead(200)
+                        response.end(template)
+                    })
             })
         }
     } else if (pathname === '/create') {
         fs.readdir('./data', function (error, filelist) {
-            var title = 'WEB - create';
-            var list = templateList(filelist);
-            var template = templateHTML(title, list, `
-            <form action = "http://localhost:3000/process_create" method = "post">
-            <p><input type = "text" name = "title" placeholder="title"></p>
+            const title = 'WEB - create'
+            const list = templateList(filelist)
+            const template = templateHTML(title, list, `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
             <p>
-                <textarea name = "description" placeholder="description"></textarea>
+              <textarea name="description" placeholder="description"></textarea>
             </p>
             <p>
-                <input type = "submit">
+              <input type="submit">
             </p>
-            </form>
-        `);
+          </form>
+        `, '')
             response.writeHead(200);
             response.end(template);
-        })
+        });
+    } else if (pathname === '/create_process') {
+        let body = ''
+        request.on('data', function (data) {
+            body = body + data;
+        });
+        request.on('end', function () {
+            const post = qs.parse(body)
+            const title = post.title
+            const description = post.description
+            fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
+                response.writeHead(302, {Location: `/?id=${title}`});
+                response.end();
+            });
+        });
     } else {
         response.writeHead(404)
         response.end('Not found')
