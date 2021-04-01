06812d92495811dce6f6ddb857e6612bb1f3fa04
6h15m <s2019w35@e-mirim.hs.kr>
2021-04-01 09:40:10 +0900

    web2-38

--- main.js
+++ main.js
@@ -124,22 +124,35 @@ var app = http.createServer(function (request, response) {
                 response.end(template);
             });
         });
-    } else if(pathname === '/update_process'){
+    } else if (pathname === '/update_process') {
         var body = '';
         request.on('data', function (data) {
             body = body + data;
         });
-        request.on('end', function(){
+        request.on('end', function () {
             var post = qs.parse(body);
             var id = post.id;
             var title = post.title;
             var description = post.description;
-            fs.rename(`data/${id}`, `data/${title}`, function(error){
-                fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            fs.rename(`data/${id}`, `data/${title}`, function (error) {
+                fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
                     response.writeHead(302, {Location: `/?id=${title}`});
                     response.end();
                 })
             });
+        });
+    } else if(pathname === '/delete_process'){
+        var body = '';
+        request.on('data', function (data){
+            body = body + data;
+        });
+        request.on('end', function (){
+            var post = qs.parse(body);
+            var id = post.id;
+            fs.unlink(`data/${id}`, function(error){
+                response.writeHead(302, {Location: `/`});
+                response.end();
+            })
         });
     } else {
         response.writeHead(404);
