b0071cf91080104506564ae408c09d5943bd7d8b
6h15m <s2019w35@e-mirim.hs.kr>
2021-05-26 09:53:56 +0900

    web3-14.1

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ app.get('*', function (request, response, next) {
     });
 });
 
+
+
 //route, routing
 //app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', function (request, response) {
@@ -25,46 +27,20 @@ app.get('/', function (request, response) {
     var description = 'Hello, Node.js';
     var list = template.list(request.list);
     var html = template.HTML(title, list,
-        `<h2>${title}</h2>${description}
+        `
+        <h2>${title}</h2>${description}
         <img src="images/hello.jpg" style="width:300px; display: block; margin-top: 10px;">
         `,
-        `<a href="/create">create</a>`
+        `<a href="/topic/create">create</a>`
     );
     response.send(html);
 });
 
-app.get('/page/:pageId', function (request, response, next) {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8',
-        function (err, description) {
-            if (err) {
-                next(err);
-            } else {
-                var title = request.params.pageId;
-                var sanitizedTitle = sanitizeHtml(title);
-                var sanitizedDescription = sanitizeHtml(description, {
-                    allowedTags: ['h1']
-                });
-                var list = template.list(request.list);
-                var html = template.HTML(sanitizedTitle, list,
-                    `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                    ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-                );
-                response.send(html);
-            }
-        });
-});
-
-app.get('/create', function (request, response) {
+app.get('/topic/create', function (request, response) {
     var title = 'WEB - create';
     var list = template.list(request.list);
     var html = template.HTML(title, list, `
-       <form action = "/create_process" method="post">
+       <form action = "/topic/create_process" method="post">
        <p><input type="text" name="title" placeholder="title"></p>
        <p>
         <textarea name="description" placeholder="description"></textarea>
@@ -77,24 +53,23 @@ app.get('/create', function (request, response) {
     response.send(html);
 });
 
-app.post('/create_process', function (request, response) {
+app.post('/topic/create_process', function (request, response) {
     var post = request.body;
     var title = post.title;
     var description = post.description;
     fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-        response.writeHead(302, {Location: `/?id${title}`});
-        response.end();
+        response.redirect(`/topic/${title}`);
     });
 });
 
-app.get('/update/:pageId', function (request, response) {
+app.get('/topic/update/:pageId', function (request, response) {
     var filteredId = path.parse(request.params.pageId).base;
     fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
         var title = request.params.pageId;
-        var list = template.list(filelist);
+        var list = template.list(request.list);
         var html = template.HTML(title, list,
             `
-                <form action="/update_process" method="post">
+                <form action="/topic/update_process" method="post">
                     <input type="hidden" name="id" value="${title}">
                     <p><input type="text" name="title" placeholder="title" value="${title}"></p>
                     <p>
@@ -105,25 +80,25 @@ app.get('/update/:pageId', function (request, response) {
                     </p>
                 </form>
                 `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
         );
         response.send(html);
     });
 });
 
-app.post('/update_process', function (request, response) {
+app.post('/topic/update_process', function (request, response) {
     var post = request.body;
     var id = post.id;
     var title = post.title;
     var description = post.description;
     fs.rename(`data/${id}`, `data/${title}`, function (error) {
         fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-            response.redirect(`/?id=${title}`);
+            response.redirect(`/topic/${title}`);
         })
     });
 });
 
-app.post('/delete_process', function (request, response) {
+app.post('/topic/delete_process', function (request, response) {
     var post = request.body;
     var id = post.id;
     var filteredId = path.parse(id).base;
@@ -131,6 +106,39 @@ app.post('/delete_process', function (request, response) {
         response.redirect('/');
     });
 });
+
+app.get('/topic/:pageId', function(request, response, next) {
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+        if(err){
+            next(err);
+        } else {
+            var title = request.params.pageId;
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description, {
+                allowedTags:['h1']
+            });
+            var list = template.list(request.list);
+            var html = template.HTML(sanitizedTitle, list,
+                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+                ` <a href="/topic/create">create</a>
+          <a href="/topic/update/${sanitizedTitle}">update</a>
+          <form action="/topic/delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+            );
+            response.send(html);
+        }
+    });
+});
+
+
+
+
+
+
+
 
 app.use(function (req, res, next){
     res.status(404).send('Sorry can\'t find that!');
